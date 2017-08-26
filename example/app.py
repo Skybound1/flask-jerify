374621984026efd3db95ea784baad7b35b47eaf7
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2017-08-26 02:42:59 +0100

    added example

---
+++ example/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, json
+from werkzeug.exceptions import default_exceptions
+from flask_jerify import Jerify, jerror_handler
+
+
+app = Flask(__name__)
+
+# Use custom json error handler
+for code in default_exceptions:
+    app.errorhandler(code)(jerror_handler)
+
+jerify = Jerify(app)
+
+
+@app.route('/test', methods=['POST'])
+@jerify.request('test')
+def test():
+    return jerify.validate({'hello': 'world'}, 'test'), 200
+
+
+app.run(debug=True)
